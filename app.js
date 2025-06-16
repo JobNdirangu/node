55c60bb5dccc8712ec6f33ca59ce3bce030ee251
jaybjob27 <jobndirangu27@gmail.com>
2025-06-16 16:23:25 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// how to to import modules in node
+// you must have installed it 
+// we use npm i package_name or npm install package _name
+const chalk=require("chalk")
+
+console.log("Starting my first node project..")
+console.log(chalk.green("My green Welcome note"))
